923ddfff26ea326174ec73b2747a007769437e78
Studdnet <st.codding@gmail.com>
2019-02-28 23:41:12 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var a;
+  var b;
+  var r;
+  var count = 0; 
+  
+  for (var i = 0; i<preferences.length; i++) {
+     
+    a = preferences[i];
+    b = preferences[a-1];
+    r = preferences[b-1]; 
+        
+        if ((a != b) && (b != r) && ((r-1) == i)) {
+         count++;
+        } 
+  }
+  return (count/3); 
 };
